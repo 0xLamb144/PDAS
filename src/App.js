64c2287df97111662dd0b1673d8f7bf34e334a53
alvinjohnson <alvinjohnson144@gmail.com>
2024-06-28 15:04:26 -0400

    installed ProfileCard Props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ProfileCard from "./ProfileCard";
 function App() {
     return <>
         <div>
-            <div> Personal Digital Assistants</div>
-
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <div> Personal Digital Assets</div>
+            <hr />
+            {/*Attributes will be passed down here from each of our profile cards. (i.e ProfileCard (Components) file) */}
+            <ProfileCard title="GitHub" handle='https://github.com/0xLamb144' />
+            <ProfileCard title="LinkedIn" handle='https://www.linkedin.com/in/alvinjohnson144/' />
+            <ProfileCard title='WakaTime' handle='https://wakatime.com/@Lamb144' />
         </div>
     </>
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,13 @@
-function ProfileCard() {
-    return <>
-        <div>ProfileCard</div>
-    </>
+function ProfileCard({ title, handle }) {
+    // Destructed cards(props) for cleaner code, created new variables with the values taken from props(cards) object.
+    // const { title, handle } = cards (DOES THE EXACT SAME THING it's 100% equivalent)
+
+    return (
+        <div>
+            <div><a href={handle}>{title}</a></div>
+        </div>
+    );
+
 }
 
 export default ProfileCard;
